b89bc7f19e5c18aed641f1e9b3447fc9fec116f8
DavidUEM <davidmunoz.4e@gmail.com>
2015-02-20 17:38:54 +0100

    añadido preferencias (no funciona)

--- app/src/main/java/com/example/alex/qizit/MainActivity.java
+++ app/src/main/java/com/example/alex/qizit/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.alex.qizit;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.ParseUser;
@@ -18,7 +21,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String fontPath;
+        String fontPath2 ="fonts/LeckerliOne-Regular.ttf";
+        TextView tv = (TextView) findViewById(R.id.welcome);
+        Typeface tf= Typeface.createFromAsset(getAssets(), fontPath2);
+        tv.setTypeface(tf);
         /*
          * Creation of the Database
          */
--- app/src/main/java/com/example/alex/qizit/OptionsMenu.java
+++ app/src/main/java/com/example/alex/qizit/OptionsMenu.java
@@ -1,18 +1,49 @@
 package com.example.alex.qizit;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class OptionsMenu extends ActionBarActivity {
-
+    SharedPreferences mSharedPreferences;
+    private SharedPreferences appPrefs;
+    final static String KEY_STORAGE = "Storage";
+    private EditText editText;
+    final static String PREFERENCIA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
 
+    public OptionsMenu(Context context, SharedPreferences appPrefs) {
+        this.appPrefs = appPrefs;
 
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+
+    }
+    static {
+        PREFERENCIA = "editTextPref";
+    }
+
+    private void onClickGuardar(View v){
+        this.editText = editText;
+        appPrefs.edit()
+                .clear()
+                .putString(PREFERENCIA, this.editText.getText().toString())
+                .commit();
+        this.editText.getText().clear();
+    }
+
+    public void onClickCargar(View v){
+        String pref = appPrefs.getString(PREFERENCIA,"");
+        editText.setText(pref);
+    }
 }
