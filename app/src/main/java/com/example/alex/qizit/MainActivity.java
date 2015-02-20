a03c7cf93b681dd587db6493d89432becd9c9113
b3ddbo1 <b3ddbo1@hotmail.com>
2015-02-20 18:24:18 +0100

    share in menu list

--- app/src/main/java/com/example/alex/qizit/MainActivity.java
+++ app/src/main/java/com/example/alex/qizit/MainActivity.java
@@ -54,6 +54,15 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
+    public void testShare(){
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        String shareBody = "Here is the share content body";
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+    }
+
     public void OnClick(View v) {
 
         Intent categories = new Intent().setClass(MainActivity.this, Categories.class);
@@ -84,6 +93,10 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.logout) {
             ParseUser.logOut();
             finish();
+
+        }
+        if (id == R.id.sharebutt) {
+            testShare();
 
         }
 
